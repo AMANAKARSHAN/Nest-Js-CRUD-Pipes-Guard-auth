f388f33d8b87f1c09d3b0c27be13208b7a48b5c0
Aman <amanakarshan.sinha@primathon.in>
2022-05-06 20:02:00 +0530

    added all into app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { config } from "process";
+import { BookModule } from "./book/book.module";
+import { BookService } from "./book/book.service";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [],
+  imports: [
+    BookModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".local.env",
+      //envFilePath:".prod.env"
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [BookService],
 })
 export class AppModule {}
